b1f8e5b35f1e09e2b7d6e35a57c1e63dbd7b217e
tuhinadasgupta <32200009+tuhinadasgupta@users.noreply.github.com>
2019-03-17 20:37:36 -0400

    Update MainActivity.kt

--- app/src/main/java/com/metro/gwuexplorer/MainActivity.kt
+++ app/src/main/java/com/metro/gwuexplorer/MainActivity.kt
@@ -26,14 +26,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         stationname = findViewById(R.id.stationname)
         remember = findViewById(R.id.remember)
         go = findViewById(R.id.go)
         alert = findViewById(R.id.alert)
-
-
-
         checkedBox = findViewById(R.id.remember);
 
         AlertDialog.Builder(this)
@@ -43,40 +39,23 @@ class MainActivity : AppCompatActivity() {
             .setPositiveButton("OK"){dialog, which ->  }
             .show()
 
-
-
         if (checkedBox.isChecked()) {
-
-            // Pass the name and the file-create mode (e.g. private to our app)
             val preferences = getSharedPreferences("gwu-explorer", Context.MODE_PRIVATE)
-            // Writing to preferences (make sure you call apply)
             preferences.edit().putString("saved_stationName", stationname.text.toString()).apply()
-            // Reading from preferences, indicate default if not present
             val savedStationName = preferences.getString("saved_stationName", "")
-
         }
 
-
-        
         go.setOnClickListener {
-
                 val geocoder = Geocoder(this, Locale.getDefault())
                 val locationName = stationname.getText().toString()
-
                 val maxResults = 3
-
                 val results: List<Address> = geocoder.getFromLocationName(locationName, maxResults)
                 lateinit var addr: MutableList<String?>
-
                 if (results != null && results.size > 0) {
                     val first = results[0]
                     val firstAdd = first.getAddressLine(0)
                     addr = mutableListOf(firstAdd)
                 }
-
-
-
-
             val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.select_dialog_singlechoice)
             arrayAdapter.addAll(addr)
             AlertDialog.Builder(this)
@@ -88,30 +67,24 @@ class MainActivity : AppCompatActivity() {
                     dialog.dismiss()
                 }
                 .show()
-
             saveData();
             loadData();
-
         }
 
         alert.setOnClickListener {
             val intent2: Intent = Intent(this, AlertActivity::class.java)
-
             startActivity(intent2)
         }
-
         loadData()
         updateText()
 
     }
-   private  fun saveData ()
-    {
+   private  fun saveData (){
         val preferences = getSharedPreferences("gwu-explorer", Context.MODE_PRIVATE)
         preferences.edit().putString("SAVED_STATIONNAME", stationname.text.toString()).apply()
         preferences.edit().putBoolean("CHECKBOX",remember.isChecked).apply()
     }
-    private fun loadData ()
-    {
+    private fun loadData (){
         val preferences = getSharedPreferences("gwu-explorer", Context.MODE_PRIVATE)
         text = preferences.getString("SAVED_STATIONNAME","")
         bool1=preferences.getBoolean("CHECKBOX",false)
