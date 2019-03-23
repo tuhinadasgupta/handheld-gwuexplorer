0dd78ee4da4e01210d79abbdfd9c5a9198f8ae8b
tuhinadasgupta <32200009+tuhinadasgupta@users.noreply.github.com>
2019-03-22 20:56:46 -0400

    networking using wmata api

---
+++ app/src/main/java/com/metro/gwuexplorer/AlertManager.kt
@@ -0,0 +1,70 @@
+package com.metro.gwuexplorer
+
+import android.widget.Toast
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONObject
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+class AlertManager {
+    private val okHttpClient: OkHttpClient
+
+    init {
+        val builder = OkHttpClient.Builder()
+
+
+        builder.connectTimeout(20, TimeUnit.SECONDS)
+        builder.readTimeout(20, TimeUnit.SECONDS)
+        builder.writeTimeout(20, TimeUnit.SECONDS)
+
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        builder.addInterceptor(logging)
+        okHttpClient = builder.build()
+    }
+
+
+
+    fun retrieveAlert(
+        successCallback: (List<Alert>) -> Unit,
+        errorCallback: (Exception) -> Unit
+    ) {
+        val primaryKey = "507c1527b662401aaee3f16396982ccc"
+        val request = Request.Builder()
+            .url("https://api.wmata.com/Incidents.svc/json/Incidents")
+            .header("api_key", "$primaryKey")
+            .build()
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                errorCallback(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val alerts = mutableListOf<Alert>()
+                val responseString = response.body()?.string()
+                if (response.isSuccessful && responseString != null) {
+                    val incidents = JSONObject(responseString).getJSONArray("Incidents")
+                    for (i in 0 until incidents.length()) {
+                        val curr = incidents.getJSONObject(i)
+                        val linename = curr.getString("LinesAffected")
+                        val desc = curr.getString("Description")
+                        alerts.add(
+                            Alert(
+                                icon = "http image",
+                                lineName = linename,
+                                description =desc
+                            )
+                        )
+                    }
+                    successCallback(alerts)
+
+                } else {
+                    errorCallback(Exception("Search alerts call failed"))
+
+                }
+            }
+        })
+    }
+}
