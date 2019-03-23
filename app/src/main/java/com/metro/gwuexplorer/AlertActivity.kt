6e3a428f3cef5d66dba7ea5599ebd7849dce4b43
tuhinadasgupta <32200009+tuhinadasgupta@users.noreply.github.com>
2019-03-22 20:44:49 -0400

    Update AlertActivity.kt

--- app/src/main/java/com/metro/gwuexplorer/AlertActivity.kt
+++ app/src/main/java/com/metro/gwuexplorer/AlertActivity.kt
@@ -4,37 +4,36 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.widget.Toast
 
 class AlertActivity : AppCompatActivity() {
+
+    private val alertManager: AlertManager = AlertManager()
     private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alert)
         recyclerView = findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
-        val alert = generateFakeAlert()
-        recyclerView.adapter = AlertAdapter(alert)
-    }
-
-    private fun generateFakeAlert(): List<Alert> {
-        return listOf(
-            Alert(
-                stationName = "Metro Center ",
-                icon = "https://...."
-            ),
-            Alert(
-                stationName = "Smithsonian ",
-                icon = "https://...."
-            ),
-            Alert(
-                stationName = "Federal Triangle ",
-                icon = "https://...."
-            ),
-            Alert(
-                stationName = "Rossyln ",
-                icon = "https://...."
+        alertManager.retrieveAlert(
+            successCallback = { alerts ->
+                runOnUiThread {
+                    if (alerts.isNotEmpty()){
+                        recyclerView.adapter = AlertAdapter(alerts)
+                    }
+                    else{
+                        // defensive error check
+                        Toast.makeText(this@AlertActivity, "No Alerts to show", Toast.LENGTH_LONG).show()
+                    }
+                }
+            },
+            errorCallback = {
+                runOnUiThread {
+                    // defensive error check
+                    Toast.makeText(this@AlertActivity, "Error retrieving Tweets", Toast.LENGTH_LONG).show()
+                }
+            }
         )
-        )
-
     }
 }
