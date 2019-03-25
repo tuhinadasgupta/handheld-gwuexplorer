639813b4dfb8bec69a03ea0413369bcac241f614
tuhinadasgupta <32200009+tuhinadasgupta@users.noreply.github.com>
2019-03-24 21:35:49 -0400

    Update AlertManager.kt

--- app/src/main/java/com/metro/gwuexplorer/AlertManager.kt
+++ app/src/main/java/com/metro/gwuexplorer/AlertManager.kt
@@ -9,23 +9,18 @@ import java.util.concurrent.TimeUnit
 
 class AlertManager {
     private val okHttpClient: OkHttpClient
-
+  //setup of okhttp
     init {
         val builder = OkHttpClient.Builder()
-
-
         builder.connectTimeout(20, TimeUnit.SECONDS)
         builder.readTimeout(20, TimeUnit.SECONDS)
         builder.writeTimeout(20, TimeUnit.SECONDS)
-
         val logging = HttpLoggingInterceptor()
         logging.level = HttpLoggingInterceptor.Level.BODY
         builder.addInterceptor(logging)
         okHttpClient = builder.build()
     }
-
-
-
+    
     fun retrieveAlert(
         successCallback: (List<Alert>) -> Unit,
         errorCallback: (Exception) -> Unit
@@ -44,15 +39,8 @@ class AlertManager {
             override fun onResponse(call: Call, response: Response) {
                 val alerts = mutableListOf<Alert>()
                 val responseString = response.body()?.string()
+                //get tweets using rail incidents api returning a json object
                 if (response.isSuccessful && responseString != null) {
-//                     val incidents = JSONObject(responseString).getJSONArray("Incidents")
-//                     for (i in 0 until incidents.length()) {
-//                         alerts.add(
-//                             Alert(
-//                                 icon = "http image",
-//                                 stationName = "stationName"
-//                             )
-//                         )
                        val incidents = JSONObject(responseString).getJSONArray("Incidents")
                        for (i in 0 until incidentss.length()) {
                         val curr = incidents.getJSONObject(i)
